623a4f7d50d1253d76c35a96ee0932290309ecba
Dror Russo <dror.russo@opereto.com>
2016-12-11 19:36:01 +0200

    get local process id if not provided (relevant for process api calls)

--- pyopereto/client.py
+++ pyopereto/client.py
@@ -79,15 +79,15 @@ class OperetoClient(object):
             except Exception,e:
                 raise OperetoClientError('Failed to parse %s input file: %s'%('arguments.yaml', str(e)))
 
-
         ## TEMP: fix in agent
         for item in self.input.keys():
             try:
                 if self.input[item]=='null':
                     self.input[item]=None
                 else:
-                    mydict = json.loads(self.input[item])
-                    self.input[item]=mydict
+                    value = json.loads(self.input[item])
+                    if isinstance(value, dict):
+                        self.input[item]=value
             except:
                 pass
 
@@ -111,7 +111,10 @@ class OperetoClient(object):
         if isinstance(pids, str):
             pids = [pids]
         if not pids:
-            raise OperetoClientError('Process(s) identifiers must be provided.')
+            if self.input.get('pid'):
+                pids = self.input['pid']
+            else:
+                raise OperetoClientError('Process identifier must be provided.')
         return pids
 
 
@@ -134,6 +137,10 @@ class OperetoClient(object):
 
 
     def _call_rest_api(self, method, url, data={}, error=None, **kwargs):
+        self.logger.debug('Request: [{}]: {}'.format(method, url))
+        if data:
+            self.logger.debug('Request Data: {}'.format(data))
+
         self._connect()
         if method=='get':
             r = self.session.get(self.input['opereto_host']+url, verify=False)
@@ -153,6 +160,8 @@ class OperetoClient(object):
             response_json = r.json()
         except:
             response_json={'status': 'failure', 'message': r.reason}
+
+        self.logger.debug('Response: [{}] {}'.format(r.status_code, response_json))
 
         if response_json:
             if response_json['status']!='success':
@@ -297,13 +306,13 @@ class OperetoClient(object):
 
     @apicall
     def modify_process_property(self, key, value, pid=None):
-        if not pid and self.input.get('pid'):
-            pid = self.input['pid']
+        pid = self._get_pids(pid)
         request_data={"key" : key, "value": value}
         return self._call_rest_api('post', '/processes/'+pid+'/output', data=request_data, error='Failed to output [%s]'%key)
 
     @apicall
-    def modify_process_summary(self, pid, text):
+    def modify_process_summary(self, pid=None, text=''):
+        pid = self._get_pids(pid)
         request_data={"id" : pid, "data": str(text)}
         return self._call_rest_api('post', '/processes/'+pid+'/summary', data=request_data, error='Failed to update process summary')
 
@@ -312,42 +321,48 @@ class OperetoClient(object):
     def stop_process(self, pids, status='success'):
         if status not in process_result_statuses:
             raise OperetoClientError('Invalid process result [%s]'%status)
-        if isinstance(pids, str):
-            pids = [pids]
+        pids = self._get_pids(pids)
         for pid in pids:
             self._call_rest_api('post', '/processes/'+pid+'/terminate/'+status, error='Failed to stop process')
 
 
     @apicall
-    def get_process_status(self, pid):
+    def get_process_status(self, pid=None):
+        pid = self._get_pids(pid)
         return self._call_rest_api('get', '/processes/'+pid+'/status', error='Failed to fetch process status')
 
 
     @apicall
-    def get_process_flow(self, pid):
+    def get_process_flow(self, pid=None):
+        pid = self._get_pids(pid)
         return self._call_rest_api('get', '/processes/'+pid+'/flow', error='Failed to fetch process information')
 
 
     @apicall
-    def get_process_rca(self, pid):
+    def get_process_rca(self, pid=None):
+        pid = self._get_pids(pid)
         return self._call_rest_api('get', '/processes/'+pid+'/rca', error='Failed to fetch process information')
 
 
     @apicall
-    def get_process_info(self, pid):
+    def get_process_info(self, pid=None):
+        pid = self._get_pids(pid)
         return self._call_rest_api('get', '/processes/'+pid, error='Failed to fetch process information')
 
     @apicall
-    def get_process_log(self, pid):
+    def get_process_log(self, pid=None):
+        pid = self._get_pids(pid)
         return self._call_rest_api('get', '/processes/'+pid+'/log', error='Failed to fetch process log')
 
 
     ## deprecated, will be removed next release
-    def get_process_property(self, pid, name=None):
+    def get_process_property(self, pid=None, name=None):
+        pid = self._get_pids(pid)
         return self.get_process_properties(pid, name)
 
     @apicall
-    def get_process_properties(self, pid, name=None):
+    def get_process_properties(self, pid=None, name=None):
+        pid = self._get_pids(pid)
         res = self._call_rest_api('get', '/processes/'+pid+'/properties', error='Failed to fetch process properties')
         if name:
             try:
@@ -427,17 +442,13 @@ class OperetoClient(object):
     @apicall
     def get_process_runtime_cache(self, key, pid=None):
         value = None
-        if not pid:
-            pid = self.input.get('pid')
-        if pid:
-            value = self._call_rest_api('get', '/processes/'+pid+'/cache?key=%s'%key, error='Failed to fetch process runtime cache')
+        pid = self._get_pids(pid)
+        value = self._call_rest_api('get', '/processes/'+pid+'/cache?key=%s'%key, error='Failed to fetch process runtime cache')
         return value
 
 
     @apicall
     def set_process_runtime_cache(self, key, value, pid=None):
-        if not pid:
-            pid = self.input.get('pid')
-        if pid:
-            self._call_rest_api('post', '/processes/'+pid+'/cache', data={'key': key, 'value': value}, error='Failed to modify process runtime cache')
+        pid = self._get_pids(pid)
+        self._call_rest_api('post', '/processes/'+pid+'/cache', data={'key': key, 'value': value}, error='Failed to modify process runtime cache')
 
