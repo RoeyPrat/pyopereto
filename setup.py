5028f5142f4d8ab78650734b2e2d3beb49054762
Dror Russo <dror.russo@opereto.com>
2017-02-28 19:51:59 +0200

    add simple cli wrappers

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+HOME_DIR = os.path.expanduser("~")
 
 setup(
     name='pyopereto',
-    version='1.0.34',
+    version='1.0.35',
     author='Dror Russo',
     author_email='dror.russo@opereto.com',
     description='Opereto Python Client',
     url = 'https://github.com/opereto/pyopereto',
-    download_url = 'https://github.com/opereto/pyopereto/archive/1.0.34.tar.gz',
+    download_url = 'https://github.com/opereto/pyopereto/archive/1.0.35.tar.gz',
     keywords = [],
     classifiers = [],
     packages = ['pyopereto'],
     package_data = {},
-    scripts=['scripts/opereto'],
+    scripts=['scripts/opereto.py', 'scripts/opereto.bat', 'scripts/opereto.sh'],
     install_requires=[
         "requests > 2.7.0",
         "pyyaml",
